ca4dab38d3dce0d9e5e08d93f6956c66baa36ab3
KnightWu <wulijian722@gmail.com>
2016-02-17 17:17:02 +0800

    NEW：支持include和puzzle相互嵌套

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 /**
  * Created by lijianwu on 2015/12/24.
  */
-exports.include = require('./lib/include').include;
-exports.puz = require('./lib/puzzle').puz;
+var fs = require('fs');
+var include = require('./lib/include');
+var puzzle = require('./lib/puzzle');
+
+exports.include = include.include;
+exports.puz = puzzle.puz;
+exports.multiTag = function (pageUrl, htmlCode) {
+    if (!fs.existsSync(pageUrl)) {
+        console.warn(pageUrl, ' is not found local!');
+        return null;
+    }
+    var code = htmlCode || fs.readFileSync(pageUrl, 'utf-8');
+    code = include.parseSubs(pageUrl, code, exports.multiTag);
+    code = puzzle.parseSubs(pageUrl, code, exports.multiTag);
+    return code;
+};
